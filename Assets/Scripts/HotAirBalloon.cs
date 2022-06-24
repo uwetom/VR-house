e59062e8de6d81178c3296f61f009ea84bacd025
Tom <tom.garne@uwe.ac.uk>
2022-06-24 16:20:36 +0100

    update

---
+++ Assets/Scripts/HotAirBalloon.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotAirBalloon : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+     AudioSource audio;
+    void Start()
+    {
+         audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playSound()
+    {
+       
+        Debug.Log("play_sound!");
+            
+        if(!audio.isPlaying ){
+            audio.Play();
+        }
+
+        
+    }
+
+
+}
